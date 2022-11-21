6d9acf44f2bcc05148decf6a25052f32fb8cec98
cristian <you@example.com>
2022-11-21 16:03:09 -0300

    datatable

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.listen(3000, ()=>{
 
 });
 
+
+/*
 <<<<<<< HEAD
 =======
     function myFunction() {
@@ -29,7 +31,7 @@ app.listen(3000, ()=>{
         // Loop through all table rows, and hide those who don't match the search query
         for (i = 0; i < tr.length; i++) {
           visible = false;
-          /* Obtenemos todas las celdas de la fila, no sólo la primera */
+          /* Obtenemos todas las celdas de la fila, no sólo la primera 
           td = tr[i].getElementsByTagName("td");
           for (j = 0; j < td.length; j++) {
             if (td[j] && td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
@@ -46,4 +48,4 @@ app.listen(3000, ()=>{
       myFunction()
 
 
->>>>>>> 308df789ddd5ac444b858332c31f357a8212af9f
+*/
